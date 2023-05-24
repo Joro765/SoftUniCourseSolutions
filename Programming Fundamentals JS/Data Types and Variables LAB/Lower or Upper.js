e8046c8a4da929a07cac605e7e113a382af8fce3
Georgi <jorogeorgiev765@gmail.com>
2023-05-24 16:12:54 +0300

    Create Lower or Upper.js

---
+++ Programming Fundamentals JS/Data Types and Variables LAB/Lower or Upper.js
@@ -0,0 +1,13 @@
+function lowerUpper(char) {
+
+
+    if (char === char.toUpperCase()) {
+        console.log("upper-case");
+    } else {
+        console.log("lower-case");
+    }
+
+
+}
+
+lowerUpper('L')
