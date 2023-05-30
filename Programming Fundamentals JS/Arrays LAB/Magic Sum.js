aa7b7167727ffe8007b65d622d9217c47cb2068f
Georgi <jorogeorgiev765@gmail.com>
2023-05-30 11:51:16 +0300

    Update Magic Sum.js

--- Programming Fundamentals JS/Arrays LAB/Magic Sum.js
+++ Programming Fundamentals JS/Arrays LAB/Magic Sum.js
@@ -1,5 +1,17 @@
-function magicSum() {
+function magicSum(arr, magicNum) {
 
+
+    for (let x = 0; x < arr.length; x++) {
+        let firstNum = arr[x];
+        for (let y = x + 1; y < arr.length; y++) {
+            let secondNum = arr[y];
+
+            if ((firstNum + secondNum) === magicNum) {
+                console.log(`${firstNum} ${secondNum}`);
+            }
+        }
+    }
 }
 
-magicSum()
+magicSum([1, 2, 3, 4, 5, 6],
+    6)
