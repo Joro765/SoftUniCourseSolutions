b5c11536a74b6898a7c605d8d0f1ce907692d4fd
Georgi <jorogeorgiev765@gmail.com>
2023-06-27 13:29:35 +0300

    Create Class Vehicle.js

---
+++ Programming Fundamentals JS/Objects LAB/Class Vehicle.js
@@ -0,0 +1,15 @@
+function vehicleClass() {
+    class Vehicle {
+        constructor(type, model, parts, fuel) {
+            this.type = type;
+            this.model = model;
+            this.parts = parts;
+            this.fuel = fuel;
+            this.parts.quality = this.parts.engine * this.parts.power
+        }
+
+        drive(fuelLoss) {
+            this.fuel -= fuelLoss;
+        }
+    }
+}
