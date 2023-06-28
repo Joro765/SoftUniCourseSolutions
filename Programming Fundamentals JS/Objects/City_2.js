d8bfba8dd67a9edb3e31ba920af70f3df38e792b
Georgi <jorogeorgiev765@gmail.com>
2023-06-28 11:33:00 +0300

    Update City_2.js

--- Programming Fundamentals JS/Objects/City_2.js
+++ Programming Fundamentals JS/Objects/City_2.js
@@ -1,12 +1,9 @@
 function cityPrint(city) {
 
-    for (const key of Object.keys(city)) {
-        {
-            console.log(`${key} -> ${city[key]}`);
-        }
+    for (const [key, value] of Object.entries(city)) {
+        console.log(`${key} -> ${value}`);
 
     }
-
 
 }
 
