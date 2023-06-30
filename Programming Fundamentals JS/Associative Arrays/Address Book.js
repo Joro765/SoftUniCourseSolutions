52e8e18f14a24ed9e9e0a7e79385d23656f81cf2
Georgi <jorogeorgiev765@gmail.com>
2023-06-30 15:05:07 +0300

    Update Address Book.js

--- Programming Fundamentals JS/Associative Arrays/Address Book.js
+++ Programming Fundamentals JS/Associative Arrays/Address Book.js
@@ -1,9 +1,12 @@
 function addressBook(data) {
+    // Create a object
     let obj = {};
 
+    // Iterate data 
     for (const info of data) {
         let [name, address] = info.split(":");
 
+        // Add to object
         if (obj.hasOwnProperty(name)) {
             obj[name] = address;
         } else {
@@ -12,10 +15,12 @@ function addressBook(data) {
 
     }
 
+
+    // Convert object to an array and sort the array
     let sortedArray = Object.entries(obj).sort(([keyA, valueA], [keyB, valueB]) => keyA.localeCompare(keyB))
 
 
-
+    // Iterate the sorted array
     for (const [key, value] of sortedArray) {
         console.log(`${key} -> ${value}`);
 
