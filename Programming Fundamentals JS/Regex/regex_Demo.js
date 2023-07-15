cf352e81db0574f716ca5c039c571536f1f51f70
Georgi <jorogeorgiev765@gmail.com>
2023-07-15 15:26:44 +0300

    Update regex_Demo.js

--- Programming Fundamentals JS/Regex/regex_Demo.js
+++ Programming Fundamentals JS/Regex/regex_Demo.js
@@ -17,4 +17,5 @@ let pattern = /[a-zA-Z]+[0-9]*[a-zA-Z0-9_]*@[a-z]+\.[a-z]+/gm;
 
 let match2 = text.match(pattern); // Вади всички записи в масив без допълнително инфо
 console.log(match2);
-console.log(pattern.test(text));
+
+console.log(pattern.test(text)); // Проверява дали имаме мач - връща true/false 
