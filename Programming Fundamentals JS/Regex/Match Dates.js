f984daec2bf30770535bfca2925bd17e555fe818
Georgi <jorogeorgiev765@gmail.com>
2023-07-17 09:35:48 +0300

    Update Match Dates.js

--- Programming Fundamentals JS/Regex/Match Dates.js
+++ Programming Fundamentals JS/Regex/Match Dates.js
@@ -1,8 +1,22 @@
-function solve() {
+function solve(text) {
+
+
+    let pattern = /(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z][a-z])\2(?<year>\d{4})/gm;
+
+    let match = pattern.exec(text);
+
+    while (match) {
+        let day = match.groups.day;
+        let month = match.groups.month;
+        let year = match.groups.year;
+
+        console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+        match = pattern.exec(text)
+    }
 
 
 }
 
 
-solve()
+solve(['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016'])
 
