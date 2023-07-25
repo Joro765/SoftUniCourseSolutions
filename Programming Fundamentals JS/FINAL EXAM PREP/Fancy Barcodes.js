ba6f9cb4407d88e66e87146f65996496648a8c95
Georgi <jorogeorgiev765@gmail.com>
2023-07-25 17:27:32 +0300

    Create Fancy Barcodes.js

---
+++ Programming Fundamentals JS/FINAL EXAM PREP/Fancy Barcodes.js
@@ -0,0 +1,65 @@
+function fancyBarcodes(data) {
+
+    // Regex to check if a BarCode is valid or not
+    let validBarcode = /([@][#]+)(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])/;
+
+    // Regex to check for numbers
+    let numbersCheck = /\d+/gm;
+
+    let barcodesCount = Number(data.shift());
+
+
+    for (const barcode of data) {
+        let match = validBarcode.exec(barcode);
+
+        if (match) {
+            let group = "";
+            let isValid = false;
+            let match2 = numbersCheck.exec(barcode);
+
+            while (match2) {
+                group += match2[0];
+                isValid = true;
+                match2 = numbersCheck.exec(barcode);
+            }
+
+            if (isValid === false) {
+                console.log(`Product group: 00`);
+            } else {
+                console.log(`Product group: ${group}`);
+            }
+
+        } else {
+            console.log(`Invalid barcode`);
+        }
+
+        match = validBarcode.exec(barcode);
+
+
+    }
+}
+
+
+fancyBarcodes((["3",
+    "@#FreshFisH@#",
+    "@###Brea0D@###",
+    "@##Che4s6E@##"]))
+
+
+
+//     for (const char of match.groups.barcode) {
+//         if (char % char === 0) {
+//             group += char;
+//             isValid = true;
+//         }
+//     }
+
+//     if (isValid === false) {
+//         console.log(`Product group: 00`);
+//     } else {
+//         console.log(`Product group: ${group}`);
+//     }
+
+// } else {
+//     console.log(`Invalid barcode`);
+// }
