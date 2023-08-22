fc0f3511e31c1d9d08dc49ffa68e520a3ac07b82
Georgi <jorogeorgiev765@gmail.com>
2023-08-22 11:23:32 +0300

    Update Same Numbers.js

--- JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Same Numbers.js
+++ JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Same Numbers.js
@@ -1,6 +1,27 @@
 function solve(num) {
 
-    console.log(num);
+    let numString = num.toString();
+
+    let currentElement = numString[0];
+
+    let isValid = true;
+    let sum = 0;
+
+    for (let element of numString) {
+        sum += Number(element)
+        if (element != currentElement) {
+            isValid = false;
+        }
+    }
+
+    if (isValid === true) {
+        console.log("true");
+        console.log(sum);
+    } else {
+        console.log("false");
+        console.log(sum);
+    }
+
 
 
 }
