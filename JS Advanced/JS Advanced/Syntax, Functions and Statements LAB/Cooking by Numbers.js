fdf8a239ae391ce935ba21f4db3442e5b8528c60
Georgi <jorogeorgiev765@gmail.com>
2023-08-29 13:00:05 +0300

    new Problem solved

--- JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Cooking by Numbers.js
+++ JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Cooking by Numbers.js
@@ -1,7 +1,44 @@
-function solve() {
+function solve(num, operation1, operation2, operation3, operation4, operation5) {
+
+    let arr = [num, operation1, operation2, operation3, operation4, operation5];
+
+    let number = Number(arr.shift())
+
+    let task = arr.shift();
+
+    while (task) {
+        switch (task) {
+            case "chop":
+                number = number / 2;
+                console.log(number);
+                break;
+            case "dice":
+                number = Math.sqrt(number);
+                console.log(number);
+                break;
+            case "spice":
+                number += 1;
+                console.log(number);
+                break;
+            case "bake":
+                number *= 3;
+                console.log(number);
+                break;
+            case "fillet":
+                number = number - (number * 0.2);
+                console.log(number);
+                break;
+        }
+        task = arr.shift();
+    }
+
+
+
+
+
 
 
 }
 
 
-solve()
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
