d8a07d643c1f0ec495f7a2fe50aa48db53bc3751
Georgi <jorogeorgiev765@gmail.com>
2023-08-30 16:28:01 +0300

    Update Validity Checker.js

--- JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Validity Checker.js
+++ JS Advanced/JS Advanced/Syntax, Functions and Statements LAB/Validity Checker.js
@@ -1,7 +1,24 @@
-function solve() {
+function solve(x1, y1, x2, y2) {
+
+    function calculate(a, b, c, d) {
+        let validator = 'invalid';
+        let result = Math.sqrt((c - a) ** 2 + (d - b) ** 2);
+        if (Number.isInteger(result)) {
+            validator = 'valid';
+        } else {
+            validator = 'invalid';
+        }
+
+        console.log(`{${a}, ${b}} to {${c}, ${d}} is ${validator}`);
+    }
+
+
+    calculate(x1, y1, 0, 0);
+    calculate(x2, y2, 0, 0);
+    calculate(x1, y1, x2, y2);
 
 
 }
 
 
-solve()
+solve(2, 1, 1, 1)
