423339c3c40496ea23d4ca77b331e832d1e9a2d9
Georgi <jorogeorgiev765@gmail.com>
2023-09-18 11:45:41 +0300

    Update solve.js

--- JS Advanced/JS Advanced/Arrays, Nested Arrays/solve.js
+++ JS Advanced/JS Advanced/Arrays, Nested Arrays/solve.js
@@ -1,6 +1,23 @@
-let nums = [17, 100, 3, 9, 28, 30, 2, 10, 0];
+let numbers = [2, 6, 10, 3, 5, 7, 22, 31, 32, 33, 40, 17, 0];
 
-nums.sort((a, b) => b - a);
+if (numbers.some(n => n > 30)) { // Проверява дали има поне един елемент в масива, който отговаря на условието
+    let el = numbers.find(n => n > 30); // Намира първия елемент в масива, който отговаря на условието
+    let indexOfEl = numbers.indexOf(el);
+    numbers.splice(indexOfEl, 1);
+
+} else {
+    console.log("There isn't an element bigger than 30");
+}
 
 
-console.log(nums);
+console.log(numbers);
+
+
+// Само четни числа с филтър
+let result = numbers.filter(x => x % 2 === 0);
+console.log(result);
+
+
+
+// Трие първия елемент над 30 който намери в масива !!!
+
