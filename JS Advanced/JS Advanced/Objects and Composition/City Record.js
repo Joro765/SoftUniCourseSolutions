67e3abf43091c31e03044ef25c55b15199211511
Georgi <jorogeorgiev765@gmail.com>
2023-09-19 14:05:22 +0300

    New Problems solved

---
+++ JS Advanced/JS Advanced/Objects and Composition/City Record.js
@@ -0,0 +1,19 @@
+function solve(city, population, treasury) {
+
+    let obj = {
+        name: city,
+        population: population,
+        treasury: treasury
+    }
+
+    return obj;
+
+}
+
+
+
+solve('Tortuga',
+
+    7000,
+
+    15000)
---
+++ JS Advanced/JS Advanced/Objects and Composition/solve.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "Georgi",
+    city: "Stara Zagora",
+    age: 27
+}
+
+
+console.log(`${person.name} is from ${person.city} and is ${person.age} years old.`);
