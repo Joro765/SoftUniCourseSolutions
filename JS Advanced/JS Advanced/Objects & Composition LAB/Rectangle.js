c536dd5d3e6a1f9dc0c99d933c1138f00f36c282
Georgi <jorogeorgiev765@gmail.com>
2023-09-24 16:53:50 +0300

    Update Rectangle.js

---
+++ JS Advanced/JS Advanced/Objects & Composition LAB/Rectangle.js
@@ -0,0 +1,23 @@
+function rectangle(width, height, color) {
+    color = color[0].toUpperCase() + color.slice(1)
+    return {
+        width: width,
+        height: height,
+        color: color,
+
+        calcArea() {
+            return this.width * this.height;
+        }
+    }
+}
+
+
+let rect = rectangle(4, 5, 'red');
+
+console.log(rect.width);
+
+console.log(rect.height);
+
+console.log(rect.color);
+
+console.log(rect.calcArea());
