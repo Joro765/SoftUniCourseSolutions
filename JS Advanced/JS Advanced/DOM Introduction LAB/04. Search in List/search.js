d72bad5f271eec81cce387ecc73e7a5771860733
Georgi <jorogeorgiev765@gmail.com>
2023-09-26 16:51:00 +0300

    New Problem Solved

--- JS Advanced/JS Advanced/DOM Introduction LAB/04. Search in List/search.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.querySelectorAll("#towns li")); // Collection
+   let search = document.getElementById("searchText").value;
+   let result = document.getElementById("result");
+
+   let counter = 0;
+
+   towns.forEach(town => {
+      if (town.textContent.includes(search)) {
+         town.style.textDecoration = "underline";
+         town.style.fontWeight = "bold";
+         counter++;
+      } else {
+         town.style.textDecoration = "none";
+         town.style.fontWeight = "";
+      }
+   })
+
+
+   result.textContent = `${counter} matches found`;
+
 }
--- JS Advanced/JS Advanced/DOM Introduction LAB/05. Table - Search Engine/solution.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/05. Table - Search Engine/solution.js
@@ -1,8 +1,32 @@
 function solve() {
+   // Table itteration by rows
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let searchField = document.getElementById("searchField");
+   let tableRow = Array.from(document.querySelectorAll("tbody tr"));
 
    function onClick() {
-      //   TODO:
+      let searchText = searchField.value;
 
+      // Итерираме по ред на таблицата
+      for (let row of tableRow) {
+
+         // Взимаме всички колони от конкретния ред
+         let rowData = row.querySelectorAll("td");
+
+         // Итерираме по колоните на реда
+         for (let td of rowData) {
+            if (searchText === "") {   // Ако въведем празен стринг демек нищо да не селектира
+               row.classList.remove("select")
+            } else if (td.textContent.includes(searchText)) {
+               // Добавяме клас на конкретния ред
+               row.classList.add("select")
+               break;  // Брейк за да не продължава търсенето na същия ред ако вече сме намерили матч
+            } else {
+               row.classList.remove("select")
+            }
+         }
+
+      }
+      searchField.value = "";
    }
 }
