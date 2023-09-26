88b72b8fb6b4cffac5c4dc7093e45a88953e29b8
Georgi <jorogeorgiev765@gmail.com>
2023-09-26 18:00:50 +0300

    Update solution.js

--- JS Advanced/JS Advanced/DOM Introduction LAB/06. Format the Text/solution.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/06. Format the Text/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO
+  let text = document.getElementById("input").value;
+  let target = document.getElementById("output");
+
+  let sentences = text.split(".");
+
+  let buff = "";
+  let count = 0;
+  for (let sentence of sentences) {
+
+    if (sentence.length > 0) {
+      buff += `${sentence}.`
+      count++;
+    } else {
+      continue;
+    }
+
+
+    if (count == 3) {
+      target.innerHTML += `<p>${buff}</p>`;
+      buff = "";
+      count = 0;
+    }
+
+
+  }
+  target.innerHTML += `<p>${buff}</p>`;
+
 }
