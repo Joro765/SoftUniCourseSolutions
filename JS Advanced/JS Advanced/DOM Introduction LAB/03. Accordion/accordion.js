02e1672dc4fb0553cf304e89aa43e50c42f2c1a2
Georgi <jorogeorgiev765@gmail.com>
2023-09-26 12:37:23 +0300

    Update accordion.js

--- JS Advanced/JS Advanced/DOM Introduction LAB/03. Accordion/accordion.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.getElementsByClassName("button")[0]; // Слагаме [0] защото връща колекция и трябва да вземем бутонът от нея като масив е 
+    btn.textContent = btn.textContent === "More" ? "Less" : "More"; // Ако е на More става на Less ако е на Less става на More -- toggle FUNCTIONALLITY
+
+    let moreInfo = document.getElementById("extra")
+
+    // В зависимост от стейта на бутона показвам или скривам текста
+    if (btn.textContent === "Less") {
+        moreInfo.style.display = "block"
+    } else {
+        moreInfo.style.display = "none"
+    }
 }
