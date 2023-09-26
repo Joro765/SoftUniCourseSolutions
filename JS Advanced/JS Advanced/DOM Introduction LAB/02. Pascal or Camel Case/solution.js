1e69d199225b13d880dba26d9dc2d8dec1fa2e31
Georgi <jorogeorgiev765@gmail.com>
2023-09-26 10:54:44 +0300

    Update solution.js

--- JS Advanced/JS Advanced/DOM Introduction LAB/02. Pascal or Camel Case/solution.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-  //TODO...
+  let originalText = document.getElementById("text").value.toLowerCase();
+  let convention = document.getElementById("naming-convention").value;
+
+  let result = [];
+  let buff = "";
+
+  switch (convention) {
+    case "Camel Case":
+      for (let i = 0; i < originalText.length; i++) {
+        if (originalText[i] !== " ") {
+          buff += `${originalText[i]}`
+        } else {
+          result.push(buff);
+          buff = "";
+          i++;
+          buff += `${originalText[i].toUpperCase()}`;
+        }
+      }
+      result.push(buff)
+      break;
+
+
+    case "Pascal Case":
+      for (let i = 0; i < originalText.length; i++) {
+        if (originalText[i] !== " ") {
+          buff += `${originalText[i]}`
+        } else {
+          buff = buff[0].toUpperCase() + buff.slice(1);
+          result.push(buff);
+          buff = "";
+          i++;
+          buff += `${originalText[i].toUpperCase()}`;
+        }
+      }
+      result.push(buff)
+      break;
+
+    default: result.push("Error!");
+  }
+
+
+  document.getElementById("result").textContent = result.join("")
 }
