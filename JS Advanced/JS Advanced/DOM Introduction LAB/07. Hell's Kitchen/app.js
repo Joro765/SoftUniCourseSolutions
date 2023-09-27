496e92de6798966fecea7bbe282feea10d2aa8b3
Georgi <jorogeorgiev765@gmail.com>
2023-09-27 14:05:14 +0300

    New Problem solved

--- JS Advanced/JS Advanced/DOM Introduction LAB/07. Hell's Kitchen/app.js
+++ JS Advanced/JS Advanced/DOM Introduction LAB/07. Hell's Kitchen/app.js
@@ -1,8 +1,26 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+   let textArea = document.querySelector("#inputs textarea");
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let info = textArea.value;
+      let infoString = JSON.parse(info)
+      let res = {};
+      for (let line of infoString) {
+         let [restaurantName, workers] = line.split(" - ");
+         if (!res.hasOwnProperty(restaurantName)) {
+            res[restaurantName] = {
+               bestSalary: calcBestSalary(),
+               averageSalary: calcAverageSalary(),
+               workers
+            }
+
+         } else {
+            res[restaurantName].workers = calcWorkers(),
+               res[restaurantName].bestSalary = calcBestSalary(),
+               res[restaurantName].averageSalary = calcAverageSalary()
+         }
+
+      }
    }
 }
--- JS Advanced/JS Advanced/DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputText = document.getElementById("newItemText");
+    let target = document.getElementById("items");
+
+    let newElement = document.createElement("li");
+
+    newElement.textContent = inputText.value;
+
+    target.appendChild(newElement);
+    inputText.value = "";
 }
