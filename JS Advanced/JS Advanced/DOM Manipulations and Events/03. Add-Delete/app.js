ba57dbc72336437199c34bf0187334a11e66ee9d
Georgi <jorogeorgiev765@gmail.com>
2023-09-27 19:46:53 +0300

    Update app.js

--- JS Advanced/JS Advanced/DOM Manipulations and Events/03. Add-Delete/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,35 @@
 function addItem() {
-    //TODO...
+    let newElement = document.getElementById("newItemText").value;
+    let list = document.getElementById("items");
+
+    // Валидация за празен стринг
+    if (newElement.length < 1) {
+        return;
+    }
+
+    // Създавам нов LI tag и му давам стойността от инпута
+    let listItem = document.createElement('li');
+    listItem.textContent = newElement;
+
+    // Създаваме REMOVE линк, който ще може да трие listItem-a
+    let removeLink = document.createElement("a");
+
+    // Добавяме текста към елемента и му закачаме евент
+    removeLink.textContent = "[Delete]"
+    removeLink.href = "#";
+    removeLink.addEventListener("click", deleteItem);
+
+    // Добавяме REMOVE елемента в LI елемента
+    listItem.appendChild(removeLink);
+
+    // Добавяме LI елемента в бодито
+    list.appendChild(listItem);
+
+    // Функцията, която трие
+    function deleteItem() {
+        listItem.remove()
+    }
+
+    // Зануляваме инпута след въвеждане
+    document.getElementById("newItemText").value = ""
 }
