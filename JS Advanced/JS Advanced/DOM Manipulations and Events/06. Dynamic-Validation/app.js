6b45de40626c78ab1f991999d3368df5cf4d7890
Georgi <jorogeorgiev765@gmail.com>
2023-09-27 23:27:25 +0300

    Update app.js

--- JS Advanced/JS Advanced/DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let pattern = /[a-z]+@[a-z]+.[a-z]+/gm;
+    let emailInput = document.getElementById("email");
+
+    emailInput.addEventListener("change", onChange);
+
+    function onChange() {
+        if (!(emailInput.value).match(pattern)) {
+            emailInput.className = "error"
+        } else {
+            emailInput.className = ""
+        }
+    }
+
+
+
 }
