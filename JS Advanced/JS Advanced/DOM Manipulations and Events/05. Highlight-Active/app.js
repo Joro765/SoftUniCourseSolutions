fe2eb992f3a215eef323a0b90487920e75e8099f
Georgi <jorogeorgiev765@gmail.com>
2023-09-27 21:49:07 +0300

    Update app.js

--- JS Advanced/JS Advanced/DOM Manipulations and Events/05. Highlight-Active/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    // Взимаме всички инпути и ги правим на масив
+    const inputFields = Array.from(document.getElementsByTagName("input"));
+
+    // За всеки инпут от масива му закачаме евент лисънъри за 2та стейта
+    inputFields.forEach(input => {
+        input.addEventListener('focus', inputFocus);
+        input.addEventListener('blur', inputBlur);
+    });
+
+    // e - ни носи информацията за моментно таргетирания елемент. Така си взимаме кой е елемента който е за обработка
+    // Сменяме му класа в зависимост от евента
+    function inputFocus(e) {
+        e.target.parentNode.className = "focused"
+    }
+
+    function inputBlur(e) {
+        e.target.parentNode.className = ""
+    }
 }
