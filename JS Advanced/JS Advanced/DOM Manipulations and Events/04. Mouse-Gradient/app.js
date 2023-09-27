b5dfc6a41de06f3f785a63c5562f53cdeb7b4733
Georgi <jorogeorgiev765@gmail.com>
2023-09-27 20:47:27 +0300

    Update app.js

--- JS Advanced/JS Advanced/DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,23 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById("gradient");
+    gradient.addEventListener("mousemove", gradientMove);
+    gradient.addEventListener("mouseout", gradientOut);
+
+
+    // Имаме параметър защото искаме да вземем допълнително инфо от браузера за това къде се намира мишката
+    function gradientMove(event) {
+        const gradientBoxWidth = event.target.clientWidth;
+        const positionOfTheMouse = event.offsetX / (gradientBoxWidth - 1);
+        const percentage = Math.trunc(positionOfTheMouse * 100);
+        document.getElementById("result").textContent = percentage + "%";
+
+    }
+
+    // Зануляваме процентите ако мишката не е върху елемента
+    function gradientOut() {
+        document.getElementById("result").textContent = "";
+    }
+
+
+
 }
