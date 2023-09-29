b78ef3b066104278a136e76fc9f38240304ddec3
Georgi <jorogeorgiev765@gmail.com>
2023-09-29 09:35:19 +0300

    Small optimisation

--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/01. Sections/script.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+   const divWrapper = document.getElementById("content");
+
+   // За всяка дума създаваме див и параграф - закачаме параграфа на див-а и ги добавяме в мейн див контейнера
+   for (const word of words) {
+      let currentP = document.createElement("p");
+      currentP.textContent = word;
+      currentP.style.display = "none";
+      let currentDiv = document.createElement("div");
+      currentDiv.appendChild(currentP);
+
+      // Закачаме към всеки див - евент лисънър
+      currentDiv.addEventListener("click", showMe);
+
+      divWrapper.appendChild(currentDiv)
+
+   }
+
+   // Функцията, получава аргумент евент, който използваме за да реферираме към див-а от когото е активиран евентът.
+   function showMe(event) {
+      event.currentTarget.children[0].style.display = "block"
+   }
 }
--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/02. Time-Converter/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/02. Time-Converter/solution.js
@@ -1,4 +1,57 @@
 function attachEventsListeners() {
+    let daysButton = document.getElementById("daysBtn");
+    let hoursButton = document.getElementById("hoursBtn");
+    let minutesButton = document.getElementById("minutesBtn");
+    let secondsButton = document.getElementById("secondsBtn");
 
-    console.log('TODO:...');
+    let daysField = document.getElementById("days");
+    let hoursField = document.getElementById("hours");
+    let minutesField = document.getElementById("minutes");
+    let secondsField = document.getElementById("seconds");
+
+
+
+    daysButton.addEventListener("click", calcDays);
+    hoursButton.addEventListener("click", calcHours);
+    minutesButton.addEventListener("click", calcMinutes);
+    secondsButton.addEventListener("click", calcSeconds);
+
+
+    let days = 0;
+    let hours = 0;
+    let mins = 0;
+    let secs = 0;
+
+
+    function calcDays() {
+        days = Number(daysField.value);
+
+        hoursField.value = days * 24;
+        minutesField.value = hoursField.value * 60;
+        secondsField.value = minutesField.value * 60;
+    }
+
+    function calcHours() {
+        hours = Number(hoursField.value);
+
+        minutesField.value = hours * 60;
+        secondsField.value = minutesField.value * 60;
+        daysField.value = hours / 24;
+    }
+
+    function calcMinutes() {
+        mins = Number(minutesField.value);
+
+        secondsField.value = mins * 60;
+        hoursField.value = mins / 60;
+        daysField.value = hoursField.value / 24;
+    }
+
+    function calcSeconds() {
+        secs = Number(secondsField.value);
+
+        minutesField.value = secs / 60;
+        hoursField.value = minutesField.value / 60;
+        daysField.value = hoursField.value / 24;
+    }
 }
