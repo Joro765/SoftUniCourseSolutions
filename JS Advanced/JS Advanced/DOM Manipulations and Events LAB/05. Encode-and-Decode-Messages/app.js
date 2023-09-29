75c2c2f5bf376c851566eb430affa096fae677c7
Georgi <jorogeorgiev765@gmail.com>
2023-09-29 18:33:12 +0300

    New Problems Solved

--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,43 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const initialTextArea = document.querySelectorAll("textarea")[0];
+    const resultTextArea = document.querySelectorAll("textarea")[1];
+    const sendButton = document.querySelectorAll("button")[0];
+    const translateButton = document.querySelectorAll("button")[1];
+
+    sendButton.addEventListener("click", encriptMessage);
+    translateButton.addEventListener("click", translateMessage)
+
+    let encriptetMessage = "";
+    let resultMessage = "";
+
+
+
+    function encriptMessage() {
+        let msg = initialTextArea.value
+        let encriptetMessage = "";
+        for (let i = 0; i < msg.length; i++) {
+            let current = msg.charCodeAt(i);
+            current += 1;
+            encriptetMessage += `${String.fromCharCode(current)}`
+        }
+
+        resultTextArea.value = encriptetMessage;
+        initialTextArea.value = "";
+    }
+
+    function translateMessage() {
+        let msg = resultTextArea.value;
+        let decriptetMessage = "";
+        for (let i = 0; i < msg.length; i++) {
+            let current = msg.charCodeAt(i);
+            current -= 1;
+            decriptetMessage += `${String.fromCharCode(current)}`
+        }
+
+        resultTextArea.value = decriptetMessage;
+
+    }
+
+
+
 }
--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/06. Furniture/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/06. Furniture/solution.js
@@ -1,4 +1,116 @@
 function solve() {
+  const initialTextArea = document.querySelectorAll("textarea")[0];
+  const getListButton = document.querySelectorAll("button")[0];
+  const table = document.querySelector("tbody");
 
-  //TODO...
+  getListButton.addEventListener("click", addElement);
+
+  function addElement(e) {
+
+    let elementsList = JSON.parse(initialTextArea.value);
+    for (const element of elementsList) {
+      let product = element; // Object with properties
+
+      // Table row
+      let trElement = document.createElement("tr");
+
+      // Image TD
+      let tdElement1 = document.createElement("td");
+      let imgElement = document.createElement("img");
+      imgElement.setAttribute("src", product.img)
+      tdElement1.appendChild(imgElement);
+
+      // Name TD
+      let tdElement2 = document.createElement("td");
+      let pNameElement = document.createElement("p");
+      pNameElement.textContent = product.name;
+      tdElement2.appendChild(pNameElement);
+
+      // Price TD
+      let tdElement3 = document.createElement("td");
+      let pPriceElement = document.createElement("p");
+      pPriceElement.textContent = product.price;
+      tdElement3.appendChild(pPriceElement);
+
+      // Decoration TD
+      let tdElement4 = document.createElement("td");
+      let pDecorationElement = document.createElement("p");
+      pDecorationElement.textContent = product.decFactor;
+      tdElement4.appendChild(pDecorationElement);
+
+      // Checkbox TD
+      let tdElement5 = document.createElement("td");
+      let pCheckBoxElement = document.createElement("input");
+      pCheckBoxElement.setAttribute("type", "checkbox");
+      pCheckBoxElement.setAttribute("disabled", "");
+      tdElement5.appendChild(pCheckBoxElement);
+
+      trElement.appendChild(tdElement1);
+      trElement.appendChild(tdElement2);
+      trElement.appendChild(tdElement3);
+      trElement.appendChild(tdElement4);
+      trElement.appendChild(tdElement5);
+
+      table.appendChild(trElement)
+
+    }
+
+    const radioButtons = Array.from(document.querySelectorAll("input"));
+
+    for (const radioButton of radioButtons) {
+      radioButton.disabled = false
+      radioButton.addEventListener("click", markItem);
+    }
+
+
+    let productsBuy = [];
+    let total = 0;
+    let avgDecFactor = 0;
+
+
+
+    // RADIO BUTTONS КОИТО РАБОТЯТ!!!!!
+    function markItem(e) {
+      if (!(e.currentTarget.hasAttribute('checked'))) {
+        e.currentTarget.setAttribute('checked', "");
+      } else {
+        e.currentTarget.removeAttribute('checked', "");
+      }
+
+    }
+
+
+
+
+    const resultTextArea = document.querySelectorAll("textarea")[1];
+    const buyButton = document.querySelectorAll("button")[1];
+    buyButton.addEventListener("click", buyItems);
+
+    function buyItems(e) {
+      const markedProducts = Array.from(document.querySelectorAll("input[type=checkbox]"));
+
+      for (const product of markedProducts) {
+        if (product.checked === true) {  //// ПРОВЕРКА ЗА СТЕЙТ НА РАДИО БУТОН !!!!!
+          let productInfo = product.parentElement.parentElement;
+          let productInfoSelector = productInfo.querySelectorAll("p");
+          let productName = productInfoSelector[0].textContent;
+          let productPrice = productInfoSelector[1].textContent;
+          let productDecoration = productInfoSelector[2].textContent;
+          productPrice = Number(productPrice);
+          productDecoration = Number(productDecoration)
+          productsBuy.push(productName);
+          total += productPrice;
+          avgDecFactor += productDecoration;
+        }
+
+        let text = `Bought furniture: ${productsBuy.join(", ")}\n`
+        let text2 = `Total price: ${total.toFixed(2)}\n`
+        let text3 = `Average decoration factor: ${avgDecFactor / productsBuy.length}`
+        resultTextArea.textContent = text + text2 + text3
+      }
+
+
+    }
+  }
+
 }
