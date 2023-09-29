cf960804a1074b782acc962045e2048e5f3b8c13
Georgi <jorogeorgiev765@gmail.com>
2023-09-29 14:45:26 +0300

    new problem solved

--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/03. Locked-Profile/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/03. Locked-Profile/app.js
@@ -1,3 +1,39 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profiles = Array.from(document.querySelectorAll(".profile"))
+    let state = "";
+
+    for (const profile of profiles) {
+        let showBtn = profile.querySelector("button");
+        showBtn.addEventListener("click", showInfo);
+        let checkBoxLocked = profile.querySelectorAll(".profile input[type=radio]")[0];
+        let checkBoxUnlocked = profile.querySelectorAll(".profile input[type=radio]")[1];
+        checkBoxLocked.addEventListener("click", locked);
+        checkBoxUnlocked.addEventListener("click", unlocked);
+
+    }
+
+    function showInfo(e) {
+        let hiddenInfo = e.currentTarget.previousElementSibling;
+
+
+        if (e.currentTarget.textContent === "Show more") {
+            hiddenInfo.style.display = "block"
+            e.currentTarget.textContent = "Hide it"
+        } else {
+            hiddenInfo.style.display = "";
+            e.currentTarget.textContent = "Show more"
+        }
+    }
+
+
+
+    function locked(e) {
+        let btn = e.currentTarget.parentElement.querySelector("button")
+        btn.removeEventListener("click", showInfo)
+    }
+
+    function unlocked(e) {
+        let btn = e.currentTarget.parentElement.querySelector("button")
+        btn.addEventListener("click", showInfo)
+    }
 }
--- JS Advanced/JS Advanced/DOM Manipulations and Events LAB/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/JS Advanced/DOM Manipulations and Events LAB/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,21 @@
 function addItem() {
-    console.log('TODO:...');
+    const menuElement = document.getElementById("menu");
+    const addBtn = document.querySelector("input[type=button]")
+
+
+
+    let text = document.getElementById("newItemText");
+    let value = document.getElementById("newItemValue");
+
+    const optionElement = document.createElement("option");
+    optionElement.textContent = text.value;
+    optionElement.setAttribute("value", value.value);
+
+    menuElement.appendChild(optionElement);
+
+    text.value = "";
+    value.value = "";
+
+
+
 }
