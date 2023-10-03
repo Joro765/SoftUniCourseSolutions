5ca0a58ac0d3ff167d7400f27695e4486270c381
Georgi <jorogeorgiev765@gmail.com>
2023-10-03 17:34:24 +0300

    Update Functional Sum.js

--- JS Advanced/JS Advanced/Advanced Functions LAB.js/Functional Sum.js
+++ JS Advanced/JS Advanced/Advanced Functions LAB.js/Functional Sum.js
@@ -1,10 +1,11 @@
-let f = (function () {
-    let total = 0;
-    return function sum(a) {
-        total += a;
-        sum.toString = () => total;
-        return sum;
-    }
-})();
+function add(num) {
+    let sum = num;
 
-console.log(f(1));
+    function calc(num2) {
+        sum += num2;
+        return calc;
+    }
+
+    calc.toString = function () { return sum };
+    return calc;
+}
