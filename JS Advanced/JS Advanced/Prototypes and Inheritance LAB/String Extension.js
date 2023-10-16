b97f6b7118b5dd1da102e998400dba55c1cee6f7
Georgi <jorogeorgiev765@gmail.com>
2023-10-16 11:05:33 +0300

    Update String Extension.js

--- JS Advanced/JS Advanced/Prototypes and Inheritance LAB/String Extension.js
+++ JS Advanced/JS Advanced/Prototypes and Inheritance LAB/String Extension.js
@@ -1,17 +1,17 @@
 (function solve() {
     String.prototype.ensureStart = function ensureStart(str) {
         if (this.startsWith(str)) {
-            return this
+            return this.toString()
         } else {
-            return str + " " + this
+            return str + "" + this
         }
     }
 
     String.prototype.ensureEnd = function ensureEnd(str) {
         if (this.endsWith(str)) {
-            return this
+            return this.toString()
         } else {
-            return this + " " + str
+            return this + "" + str
         }
     }
 
@@ -23,11 +23,42 @@
         }
     }
 
+
+
     String.prototype.truncate = function (n) {
+        if (n < 4) {
+            return ".".repeat(n)
+        }
+
+        if (this.length <= n) {
+            return this.toString();
+        }
+
+        if (this.includes(" ")) {
+            let words = this.split(" ");
+            let res = [];
+
+            for (const word of words) {
+                if (res.join(" ").length + word.length + 3 <= n) {
+                    res.push(word)
+                } else {
+                    break;
+                }
+            }
+            return res.join(" ") + "...";
+        }
+        return this.slice(0, n - 3) + "...";
 
     }
 
-    String.format = function format() {
+
+
+
+    String.format = function format(string, ...params) {
+        for (let i = 0; i < params.length; i++) {
+            string = string.replace(`{${i}}`, params[i]);
+        }
+        return string
 
     }
 
@@ -36,3 +67,4 @@
 
 let str = "hello";
 
+console.log(str.truncate(2));
