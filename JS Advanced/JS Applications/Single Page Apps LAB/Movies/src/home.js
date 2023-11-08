fc7e1a3886ce8256b7617f233c2e78f96ba05128
Georgi <jorogeorgiev765@gmail.com>
2023-11-08 17:07:50 +0200

    .

--- JS Advanced/JS Applications/Single Page Apps LAB/Movies/src/home.js
+++
@@ -1,7 +0,0 @@
-const main = document.querySelector("main");
-const homeSection = document.getElementById("home");
-
-
-export function showHome() {
-    main.replaceChildren(homeSection);
-}
--- JS Advanced/JS Applications/Single Page Apps LAB/Movies/src/logout.js
+++
@@ -1,8 +0,0 @@
-import { authUpdate } from "./auth.js";
-
-
-export function logout() {
-    alert("SUCCESSFUL LOGOUT")
-    sessionStorage.clear();
-    authUpdate();
-}
