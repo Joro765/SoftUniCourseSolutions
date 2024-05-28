57819b467bc0a648c88207e6676ef1ca2259e037
Georgi <jorogeorgiev765@gmail.com>
2024-05-28 17:04:53 +0300

    Add main layout

--- JS WEB/MovieWorkShop/src/controllers/aboutController.js
+++ JS WEB/MovieWorkShop/src/controllers/aboutController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get("/about", (req, res) => {
-    res.render("about", { layout: false })
+    res.render("about")
 });
 
 
--- JS WEB/MovieWorkShop/src/controllers/homeController.js
+++ JS WEB/MovieWorkShop/src/controllers/homeController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.render("home", { layout: false })
+    res.render("home")
 });
 
 
