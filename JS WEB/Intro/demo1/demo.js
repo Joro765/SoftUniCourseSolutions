fd0cc6d7cb0731e8a852b0527dbaa64d6a33d9fe
Georgi <jorogeorgiev765@gmail.com>
2024-05-28 09:49:51 +0300

    .

--- JS WEB/Intro/demo1/demo.js
+++
@@ -1,4 +0,0 @@
-const { sum, multiply } = require("./utils");
-
-sum(14, 2);
-multiply(10, 2);
--- JS WEB/Streams/demo3/readDir.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-
-fs.readdir("./folder", "utf-8")
-    .then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log("There is an error with the file system!");
-    })
