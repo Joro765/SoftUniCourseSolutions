07438c1b461e6a1fde9d05815ede1527dc0e6775
Georgi <jorogeorgiev765@gmail.com>
2024-05-28 12:48:45 +0300

    add express-handlebars

--- JS WEB/MovieWorkShop/src/index.js
+++ JS WEB/MovieWorkShop/src/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
+// Setup Handlebars
+app.engine("hbs", handlebars.engine({
+    extname: "hbs"
+}));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+
 
 app.get("/", (req, res) => {
-    res.send("Works");
+    res.render("home", { layout: false })
 });
 
 app.listen(port, () => {
