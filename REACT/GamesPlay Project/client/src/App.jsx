cc8b9165bd8140d35f89fd090644d9121c67d627
Georgi <jorogeorgiev765@gmail.com>
2024-07-22 15:55:53 +0300

    Splitted html into components

--- REACT/GamesPlay Project/client/src/App.jsx
+++ REACT/GamesPlay Project/client/src/App.jsx
@@ -1,11 +1,15 @@
-
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
 
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div id="box">
+      <Header />
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
   )
 }
 
